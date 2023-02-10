70fc5db05c7be09a4264830ef25ef403f6a0e7d8
mehrojalimov <alimovmekhroj01@gmail.com>
2023-02-10 15:39:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+monthlyIncome=0
+function getIncome() {
+  monthlyIncome = parseInt(prompt("Please enter your montly income: $", "0"));
+  if (monthlyIncome != null) {
+    document.getElementById("inc").innerHTML =
+    "This is your monthly income: $" + monthlyIncome;
+  }
+}
 
+
+
+function catergories(){
+  var necc = parseInt(.50*monthlyIncome)
+  var wants = parseInt(.30*monthlyIncome)
+  var savings = parseInt(.20*monthlyIncome)
+  document.getElementById("cat").innerHTML =
+    "This is what you should spend on neccessities this month: $"+necc;
+  document.getElementById("cat2").innerHTML =
+    "This is what you should spend on wants this month: $" + wants;
+  document.getElementById("cat3").innerHTML =
+    "This is what you should put into your savings account this month: $" + savings;
+}
