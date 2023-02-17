678934c0eb5e3ba7786834fc61c0214942227567
Bahodir Jumaboyev <bbj28@drexel.edu>
2023-02-17 09:20:37 +0000

    Update index.html, script.js Deleted upload.html

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ function catergories(){
   document.getElementById("cat3").innerHTML =
     "This is what you should put into your savings account this month: $" + savings;
 }
+
+let wantsTotal = 0;
+let needsTotal = 0;
+let savingsTotal = 0;
+      
+function submitSpending() {
+  let statement = document.getElementById("statement").value;
+  let spending = parseFloat(document.getElementById("spending").value);
+  let category = document.getElementById("category").value;
+        
+  if (category === "wants") {
+    wantsTotal += spending;
+  } else if (category === "needs") {
+    needsTotal += spending;
+  } else if (category === "savings") {
+    savingsTotal += spending;
+  }
+
+  updateSpendingSummary();
+}
+      
+function updateSpendingSummary() {
+  let total = wantsTotal + needsTotal + savingsTotal;
+  let wantsPercentage = (wantsTotal / total) * 100;
+  let needsPercentage = (needsTotal / total) * 100;
+  let savingsPercentage = (savingsTotal / total) * 100;
+        
+  document.getElementById("wants-total").innerHTML = wantsTotal.toFixed(2);
+  document.getElementById("wants-percentage").innerHTML = wantsPercentage.toFixed(2);
+  document.getElementById("needs-total").innerHTML = needsTotal.toFixed(2);
+  document.getElementById("needs-percentage").innerHTML = needsPercentage.toFixed(2);
+  document.getElementById("savings-total").innerHTML = savingsTotal.toFixed(2);
+  document.getElementById("savings-percentage").innerHTML = savingsPercentage.toFixed(2);
+}
